11bc1ca2e4b3e91a158df3d23789cb1a5e60fc0d
TomoakiYasukawa <thought.and.intention.306@gmail.com>
2022-04-12 16:49:11 +0900

    make it better

---
+++ Test.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import investpy
+import requests
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import time
+import datetime
+import talib as ta
+ta.get_function_groups
+import os
+
+os.system('git init')
+os.system('git add .')
+os.system('git commit -am "make it better"')
+os.system('git push origin master')
