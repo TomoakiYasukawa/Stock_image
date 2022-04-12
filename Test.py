6fce5b4d8504a08d36678c3004290b1111cf6770
TomoakiYasukawa <thought.and.intention.306@gmail.com>
2022-04-12 21:46:21 +0900

    make it better

--- Test.py
+++ Test.py
@@ -4,5 +4,5 @@ import os
 os.system('git init')
 os.system('git add .')
 os.system('git commit -am "make it better"')
-os.system('git remote add origin https://github.com/TomoakiYasukawa/Stock_image.git')
+os.system('git remote add origin https://github.com/TomoakiYasukawa/Stock_line.git')
 os.system('git push origin master')
